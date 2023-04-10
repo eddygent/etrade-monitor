4f09f806c304bc83a8f1866244628587ebe22e82
Kori Vernon <trapbookpro@trapbookpro.mynetworksettings.com>
2023-04-09 20:59:49 -0400

    fix

--- etrade_apps/config.py
+++
@@ -1,7 +0,0 @@
-email = "kori.s.vernon@gmail.com"
-password = "xjmzsasgkygczxsj"
-ACC_TYPE = {
-    "aaHhH dOnT tOuCH mE tHerE": "Savings",
-    "stonks": "Investing",
-    "errday spendin": "Checking"
-}
